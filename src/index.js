58363aab633d9d06bd8a47e09e1733a4af847fc2
YJ Kim <yj@wifi-172-23-72-41.uoa-wifi.auckland.ac.nz>
2018-12-10 11:55:39 +1300

    Revert "Add xIsNext state"
    This reverts commit bbe1e84a3c810eadb274cdabe580dd13c9ab04a9.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 class Square extends React.Component {
   render() {
     return(
-      <button onClick={this.props.onClick}>{this.props.value}</button>
+      <button onClick={this.props.onClick}>X</button>
     )
   }
 }
@@ -29,20 +29,11 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   state = {
-    game: Array(9).fill(null),
-    xIsNext: true,
+    game: Array(9).fill('X'),
   }
 
   handleOnClick(i) {
-    let game = this.state.game.slice();
-    const nextTurn = this.state.xIsNext? "X":"O";
-    
-    game[i] = nextTurn;
-    
-    this.setState({
-      game: game,
-      xIsNext: !this.state.xIsNext,
-    })
+    console.log("handleOnclick method excuted")
   }
 
   render() {
